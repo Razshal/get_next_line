75d8f1d0d12f2e5a58130642b698d4cac7361656
Razshal <fontenille.mathieu@gmail.com>
2017-12-08 19:15:01 +0100

    back home commit

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,36 @@
 /*   By: mfonteni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/06 16:50:37 by mfonteni          #+#    #+#             */
-/*   Updated: 2017/12/06 17:30:53 by mfonteni         ###   ########.fr       */
+/*   Updated: 2017/12/08 19:13:53 by mfonteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char *ft_realloc(char *str, char *str_to_add)
+{
+	char *copy;
+
+	if (!str || !(copy = ft_strnew(ft_strlen(str))))
+		return (NULL);
+	if (!str_to_add)
+		return (str);
+	ft_strcpy(copy, str);
+	ft_memdel((void*)&str);
+	str = ft_strnew(ft_strlen(copy) + ft_strlen(str_to_add) - 1);
+	ft_strcat(str, copy);
+	ft_strcat(str, str_to_add);
+	ft_memdel((void*)&copy);
+	return (str);
+}
+
+
+
 int get_next_line(const int fd, char **line)
 {
+	static f_list *list;
 
+	list = NULL;
+	
+	
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,23 @@
 /*   By: mfonteni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/06 17:03:53 by mfonteni          #+#    #+#             */
-/*   Updated: 2017/12/06 17:05:07 by mfonteni         ###   ########.fr       */
+/*   Updated: 2017/12/08 18:16:18 by mfonteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 #define GET_NEXT_LINE_H
+#include <stdlib.h>
+#include "libft/libft.h"
 
 int get_next_line(const int fd, char **line);
 #define BUFF_SIZE 32
+
+typedef struct	s_fd_list
+{
+	char	*str;
+	int		fd;
+	int		line;
+}				t_fd_list;
 
 #endif
